c6010c77752bcab5eb0ac6c911104a2d57bafb05
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-12-09 21:05:50 +0530

    Time: 61 ms (68.75%), Space: 43.2 MB (67.85%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,10 +3,13 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    
-	return function(x) {
-          return functions.reduceRight((res, fn) => fn(res), x);
+  return function(x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
     }
+    return x;
+  }
+    
 };
 
 /**

adf686a4f585f441f9fa5e1c7b5b5ae6745568bb
Mehek Fatima <128465778+MehekFatima@users.noreply.github.com>
2023-12-09 20:33:59 +0530

    Time: 60 ms (19.36%), Space: 44.1 MB (9.67%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+   if(nums.length==0)
+       return init;
+    let val = init;
+    for (const num of nums) {
+        val = fn(val,num); 
+    }
+
+    return val;
+}
+
+
+
